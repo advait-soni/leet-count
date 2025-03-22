41b53a64b81bc9f1b9e5b5402a15fec4795add3e
advait-soni <advaitsoni2020@gmail.com>
2025-03-22 17:11:34 +0530

    sending message to content.js

--- scripts/background.js
+++ scripts/background.js
@@ -2,27 +2,27 @@ let fetchingSubmission = new Set();
 
 chrome.webRequest.onCompleted.addListener(
   async (details) => {
+    if (fetchingSubmission.has(details.url)) return;
 
+    fetchingSubmission.add(details.url);
 
-      if (fetchingSubmission.has(details.url)) return;
+    try {
+      const response = await fetch(details.url);
+      const data = await response.json();
+      console.log("LeetCode Submission Status:", data.status_msg);
+      console.log("LeetCode question id", data.question_id);
 
-
-      fetchingSubmission.add(details.url);
-
-      try {
-        const response = await fetch(details.url);
-        const data = await response.json();
-        console.log("LeetCode Submission Status:", data);
-
+      if (data.question_id && data.status_msg) {
         chrome.tabs.sendMessage(details.tabId, {
-          submissionStatus: data,
+          submissionStatus: data.status_msg,
+          questionId: data.question_id,
         });
-      } catch (error) {
-        console.error("Error fetching submission status:", error);
-      } finally {
-        fetchingSubmission.delete(details.url);
       }
-
+    } catch (error) {
+      console.error("Error fetching submission status:", error);
+    } finally {
+      fetchingSubmission.delete(details.url);
+    }
   },
   { urls: ["https://leetcode.com/submissions/detail/*/check/"] }
 );
