b32e080dcb261695ab711ee46d7fa6c6311da2de
advait-soni <advaitsoni2020@gmail.com>
2025-03-22 17:03:09 +0530

    commit

--- scripts/background.js
+++ scripts/background.js
@@ -1,10 +1,28 @@
+let fetchingSubmission = new Set();
+
+chrome.webRequest.onCompleted.addListener(
+  async (details) => {
 
 
-chrome.webRequest.onBeforeRequest.addListener(
-    function (details) {
-        
-    },
-    { urls: ["*://leetcode.com/submissions/detail/*/check/"]} 
-)
+      if (fetchingSubmission.has(details.url)) return;
 
 
+      fetchingSubmission.add(details.url);
+
+      try {
+        const response = await fetch(details.url);
+        const data = await response.json();
+        console.log("LeetCode Submission Status:", data);
+
+        chrome.tabs.sendMessage(details.tabId, {
+          submissionStatus: data,
+        });
+      } catch (error) {
+        console.error("Error fetching submission status:", error);
+      } finally {
+        fetchingSubmission.delete(details.url);
+      }
+
+  },
+  { urls: ["https://leetcode.com/submissions/detail/*/check/"] }
+);
--- scripts/content.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener((message) => {
-    if (message.url) {
-        fetch(message.url)
-        .then(response => console.log(response))
-    }
-});
