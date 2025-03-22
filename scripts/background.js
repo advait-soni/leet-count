508ab1476a9b87fd564f69228471b595b10db911
advait-soni <advaitsoni2020@gmail.com>
2025-03-22 15:17:06 +0530

    commit

---
+++ scripts/background.js
@@ -0,0 +1,10 @@
+
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function (details) {
+        
+    },
+    { urls: ["*://leetcode.com/submissions/detail/*/check/"]} 
+)
+
+
--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,6 @@
-console.log("chrome.webRequest " , chrome.webRequest)
+chrome.runtime.onMessage.addListener((message) => {
+    if (message.url) {
+        fetch(message.url)
+        .then(response => console.log(response))
+    }
+});
