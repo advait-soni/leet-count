8152c29e650c9b471b1d6bfeafbd5eda7bf12058
advait-soni <advaitsoni2020@gmail.com>
2025-04-06 18:51:21 +0530

    pass content headers

--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,5 @@
-import { API_BASE_URL } from "../config"
+const API_BASE_URL = "http://localhost:8000"
+
 
 chrome.runtime.onMessage.addListener(message => {
   if (message.submissionStatus && message.questionId) {
@@ -6,10 +7,15 @@ chrome.runtime.onMessage.addListener(message => {
     console.log("Received question ID:", message.questionId)
     let a = new Date()
     let date = a.toDateString().slice(4)
-    let id = message.questionId
-
+    let id = message.questionId || ''
+    console.log('date', date)
+    console.log('hello')
+    console.log('id', id)
     fetch(`${API_BASE_URL}/api/questions`, {
       method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
       body: JSON.stringify({
         date: date,
         id: id,
