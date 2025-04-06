9e1c6768bffe455c421292f5dbee54c086ee0a67
advait-soni <advaitsoni2020@gmail.com>
2025-04-06 18:03:39 +0530

    post

--- scripts/content.js
+++ scripts/content.js
@@ -1,22 +1,19 @@
-import { API_BASE_URL } from "../config";
+import { API_BASE_URL } from "../config"
 
-chrome.runtime.onMessage.addListener((message) => {
-    if (message.submissionStatus && message.questionId) {
-      console.log("Received submission status:", message.submissionStatus);
-      console.log("Received question ID:", message.questionId);
-      let a = new Date()
-      let date = a.toDateString().slice(4)
-      let id = message.questionId
+chrome.runtime.onMessage.addListener(message => {
+  if (message.submissionStatus && message.questionId) {
+    console.log("Received submission status:", message.submissionStatus)
+    console.log("Received question ID:", message.questionId)
+    let a = new Date()
+    let date = a.toDateString().slice(4)
+    let id = message.questionId
 
-      fetch(`${API_BASE_URL}/api/questions`, {
-        method: 'POST', 
-        body: JSON.stringify({
-          date: date, 
-          id: id
-        })
-      })
-      
-    }
-  })
-  
-
+    fetch(`${API_BASE_URL}/api/questions`, {
+      method: "POST",
+      body: JSON.stringify({
+        date: date,
+        id: id,
+      }),
+    })
+  }
+})
