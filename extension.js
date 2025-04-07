2ac88ae8874d931db6e783ab6c1b5ff3034f9b1f
advait-soni <advaitsoni2020@gmail.com>
2025-04-07 13:28:50 +0530

    integrating questions api

---
+++ extension.js
@@ -0,0 +1,10 @@
+const API_BASE_URL = "http://localhost:8000"
+
+document.addEventListener('DOMContentLoaded', () => {
+    let a = new Date()
+    let date = a.toDateString().slice(4)
+    fetch(`${API_BASE_URL}/api/questions/${date}`).then(res => res.json()).then((data) => {
+        console.log('data', data)
+        document.getElementById('cnt').innerText = `Q's : ${data.num}`
+    })
+})
